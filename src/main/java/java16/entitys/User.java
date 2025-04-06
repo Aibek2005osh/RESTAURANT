9873a5fcc529bcba21e8ac521c9efd53861649ce
Aibek <clesh.meder.271@gmail.com>
2025-04-06 16:35:03 +0600

    A

--- src/main/java/java16/entitys/User.java
+++ src/main/java/java16/entitys/User.java
@@ -68,8 +68,7 @@ public class User implements UserDetails {
     public String getUsername() {
         return email;
     }
-
-
+    g
     @JsonIgnore
     @Override
     public boolean isAccountNonExpired() {
