52f3a33792a21139655d1a1c4eba90ac18bf53c6
Aibek <clesh.meder.271@gmail.com>
2025-04-11 10:49:16 +0600

    A

--- src/main/java/java16/apis/CategoryApi.java
+++ src/main/java/java16/apis/CategoryApi.java
@@ -5,7 +5,7 @@ import java16.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;g
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/category")
