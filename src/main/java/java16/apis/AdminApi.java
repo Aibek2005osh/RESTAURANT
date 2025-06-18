cbfebcb9c13c183bd4e6b80661bc51ddd350740b
Aibek <clesh.meder.271@gmail.com>
2025-06-18 21:41:59 +0600

    aa

--- src/main/java/java16/apis/AdminApi.java
+++ src/main/java/java16/apis/AdminApi.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/admin")
 @RequiredArgsConstructor
-
 @PreAuthorize("hasAuthority('ADMIN')")
 public class AdminApi {
 
